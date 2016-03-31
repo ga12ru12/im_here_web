8ce47b6d34a1b525acf1653cc9074ec25fd0460a
ga21ru21@gmail.com <quang123>
2016-03-31 15:37:25 +0700

    css logo

--- source/js/Components/Info.jsx
+++ source/js/Components/Info.jsx
@@ -7,8 +7,10 @@ export default class Info extends Component{
     return(
       <div className="info-div">
         <div className="header-div">
-          <div><img src="./img/default_avatar.png"></img></div>
-          <div><span><b>0976 112 600</b></span></div>
+          <div>
+            <img src="./img/logo_short.png"></img>
+            <span>I'm here</span>
+          </div>
         </div>
       </div>
     );
